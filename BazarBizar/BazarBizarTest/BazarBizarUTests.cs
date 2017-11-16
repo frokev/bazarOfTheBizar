dc66332e436844111ed3ac560aa425178d1951f4
huvkar15 <huvkar15@student.westerdals.no>
2017-11-16 13:37:48 +0100

    Laget noen tester

---
+++ BazarBizar/BazarBizarTest/BazarBizarUTests.cs
@@ -0,0 +1,79 @@
+﻿
+
+
+using ProductFactory = BazarBizar.ProductFactory;
+using Booth = BazarBizar.Booth;
+
+namespace BazarBizarTest
+{
+    using System;
+    using BazarBizar;
+    using NUnit.Framework;
+    using ProductFactory = ProductFactory;
+    using Booth = Booth;
+
+    [TestFixture]
+    public class BazarBizarUTests
+    {
+
+        #region TestFixture Config
+        [OneTimeSetUp]
+        public void InitalSetup()
+        {
+
+        }
+
+
+        [OneTimeTearDown]
+        public void FinalTearDown()
+        {
+
+        }
+
+
+        [SetUp]
+        public void SetupForEachTest()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDownForEachTest()
+        {
+
+        }
+        #endregion /TestFixture Config
+        [Test]
+        public void TestCategory()
+        {
+            Booth booth1 = new Booth("Booth1");
+
+            IProduct item = ProductFactory.GetProduct(booth1, ProductFactory.Category.Clothing, "genser", 200);
+            Assert.That(item.Category == "Clothing");
+        }
+
+        [Test]
+
+        public void TestBoothStockCount()
+        {
+            Booth booth1 = new Booth("Booth1");
+
+            booth1.AddToStock(ProductFactory.GetProduct(booth1, ProductFactory.Category.Electronic, "IphoneXxX", 12000));
+            booth1.AddToStock(ProductFactory.GetProduct(booth1, ProductFactory.Category.Electronic, "Doro", 200));
+
+            Assert.That(booth1.Stock.Count == 2);
+
+        }
+
+        [Test]
+
+        public void TestGuid()
+        {
+            string Guid1 = BazarController.GenerateUid();
+            string Guid2 = BazarController.GenerateUid();
+
+            Assert.AreNotEqual(Guid1, Guid2);
+
+        }
+    }
+}
---
+++ BazarBizar/BazarBizarTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BazarBizarTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BazarBizarTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99680dfd-70ec-4031-9b22-08a7c8c4489b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
